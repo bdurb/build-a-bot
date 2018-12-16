72f65b2a75203442771eee310359ad7bb44861b4
bdurb <brian@rpmgreenville.com>
2018-12-16 10:04:24 -0800

    cleaned up some styling on the app component

--- src/App.vue
+++ src/App.vue
@@ -18,12 +18,19 @@ export default {
 </script>
 
 <style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
 }
 </style>
