be948e77b4028c9a998f9fbb9e873c5d591309ea
bdurb <brian@rpmgreenville.com>
2018-12-17 13:52:46 -0800

    returned an axios promise so the site auto nav's to the cart when a robot is added

--- src/store/index.js
+++ src/store/index.js
@@ -23,6 +23,11 @@ export default new Vuex.Store({
       axios.get('/api/parts')
         .then(res => commit('updateParts', res.data))
         .catch(console.error)
+    },
+    addRobotToCart({ commit, state }, robot) {
+      const cart = [...state.cart, robot]
+      return axios.post('/api/cart', cart)
+        .then(() => commit('addRobotToCart', robot))
     }
   },
   getters: {
