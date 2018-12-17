8456c72d98641918818925360810d9e8ad435886
bdurb <brian@rpmgreenville.com>
2018-12-16 19:44:46 -0800

    set up route to component to show info about each part of the robot

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 
 import HomePage from '../home/HomePage.vue'
 import RobotBuilder from '../build/RobotBuilder.vue'
+import PartInfo from '../parts/PartInfo.vue'
 
 Vue.use(Router)
 
@@ -11,10 +12,16 @@ export default new Router({
     path: '/',
     name: 'Home',
     component: HomePage
-  }, {
+  },
+  {
     path: '/build',
     name: 'Build',
     component: RobotBuilder
+  },
+  {
+    path: '/parts',
+    name: 'Parts',
+    component: PartInfo
   }
 ]
 })
